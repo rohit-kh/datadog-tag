3f249b7523aa45c1d303d13b49ae7470d4ac3367
rohit-kh <rohitkhatri.in@gmail.com>
2022-02-09 11:43:47 +0530

    Added two endpoints

--- web/app.py
+++ web/app.py
@@ -15,17 +15,25 @@ tracer.configure(
 app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
 
-@app.route('/')
-def hello():
-    # Increment the Datadog counter.
-    statsd.increment('docker_compose_example.page.views')
 
+@app.route('/supply_branding')
+def supply_branding():
     current_span = tracer.current_span()
     if current_span:
-        current_span.set_tag('customer', 'customer-name')
+        current_span.set_tag('product', 'supply_branding')
+    redis.incr('supply.branding.view')
+    return 'Hello World! I have been seen %s times.' % redis.get('supply.branding.view')
 
-    redis.incr('hits')
-    return 'Hello World! I have been seen %s times.' % redis.get('hits')
+
+
+@app.route('/supply_storefront')
+def supply_storefront():
+    current_span = tracer.current_span()
+    if current_span:
+        current_span.set_tag('product', 'supply_storefront')
+    redis.incr('supply.storefront.view')
+    return 'Hello World! I have been seen %s times.' % redis.get('supply.storefront.view')
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
